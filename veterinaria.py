ac875a25e7c863564150ed9a279f7bb087a7f2ba
malessan4 <matialess@gmail.com>
2025-09-05 00:03:20 -0300

    se mejora y ahora se puede guardar en memoria mientras el archivo se ejecuta

--- veterinaria.py
+++ veterinaria.py
@@ -1,19 +1,18 @@
-veterinaria_lazaro = []
+veterinaria_lazaro = []  
 
-def carga():
-    lista = []  
-    cod = int(input("Ingrese el codigo ID de la mascota: "))
-    while cod != 0:
-        nom = input("Ingrese el nombre de la mascota: ")
+def agregar_mascota():
+    id = int(input("Ingrese el codigo ID de la mascota: "))
+    while id != 0:
+        nombre = input("Ingrese el nombre de la mascota: ")
         edad = int(input("Ingrese la edad de la mascota: "))
         tipo = input("Ingrese el tipo de mascota: ")
         raza = input("Ingrese la raza de la mascota: ")
         sexo = input("Ingrese el sexo de la mascota (M/H): ")
         atencion = input("Ingrese el tipo de atencion (baño, cirugia, internacion, chequeo): ")
-        li = [cod, nom, edad, tipo, raza, sexo, atencion]
-        lista.append(li)
-        cod = int(input("Ingrese el codigo ID de la mascota (0 para terminar): "))
-    return lista 
+        li = [id, nombre, edad, tipo, raza, sexo, atencion]
+        veterinaria_lazaro.append(li)
+        id = int(input("Ingrese el codigo ID de la mascota (0 para terminar): "))
+    return veterinaria_lazaro 
 
 # Programa principal
         
@@ -34,7 +33,7 @@ while op != 4:
     if op == 1:
         print(veterinaria_lazaro)  
     elif op == 2:
-        nueva_mascota = carga()
+        nueva_mascota = agregar_mascota()
         veterinaria_lazaro.append(nueva_mascota)  
     elif op == 3:
         codigo_buscar = int(input("Ingrese el codigo ID de la mascota a buscar: "))
