c6da040d7aaa0538ad81a92a0a8235b80f67a3a2
malessan4 <matialess@gmail.com>
2025-09-05 09:37:50 -0300

    se estructura mejor el programa, en el progama principla solo se invoca a las funciones

--- veterinaria-alessandrello.py
+++ veterinaria-alessandrello.py
@@ -9,7 +9,8 @@ base_datos= [
         {'id': 4, 'nombre': 'Rex', 'edad': 5, 'especie': 'Perro', 'raza': 'Pastor Alemán', 'sexo': 'M', 'atencion': 'Cirugia', 'dueño': 'Ana Rodríguez'},
         {'id': 5, 'nombre': 'Nala', 'edad': 4, 'especie': 'Gato', 'raza': 'Persa', 'sexo': 'F', 'atencion': 'Chequeo', 'dueño': 'Pedro Sánchez'},
         {'id': 6, 'nombre': 'Bobby', 'edad': 2, 'especie': 'Perro', 'raza': 'Chihuahua', 'sexo': 'M', 'atencion': 'Baño', 'dueño': 'Laura Martínez'},
-        {'id': 7, 'nombre': 'Luna', 'edad': 3, 'especie': 'Gato', 'raza': 'Angora', 'sexo':'F', 'atencion': 'Cirugia',  'dueño': 'David Torres'}
+        {'id': 7, 'nombre': 'Luna', 'edad': 3, 'especie': 'Gato', 'raza': 'Angora', 'sexo':'F', 'atencion': 'Cirugia',  'dueño': 'David Torres'},
+        {'id': 8, 'nombre': 'Max', 'edad': 6, 'especie': 'Gato', 'raza': 'Persa', 'sexo': 'M', 'atencion': 'Cirugia',  'dueño': 'Sofía Ramírez'},
     ]
 
 def limpiar_terminal():
@@ -56,6 +57,41 @@ def listar_mascotas():
         sexo_str = 'Macho' if mascota[5] == 'M' else 'Hembra'
         print(f"{mascota[0]:<5} {mascota[1]:<15} {mascota[2]:<5} {mascota[3]:<10} {mascota[4]:<15} {sexo_str:<10} {mascota[6]:<12} {mascota[7]:<15}")
 
+def buscar_mascota_id():
+        codigo_buscar = int(input("Ingrese el codigo ID de la mascota a buscar: "))
+        encontrado = False
+        
+        for mascota in veterinaria_alessandrello:
+            if not encontrado and mascota[0] == codigo_buscar:
+                print("\n--- HISTORIA CLÍNICA ---")
+                print(f"ID: {mascota[0]}")
+                print(f"Nombre: {mascota[1]}")
+                print(f"Edad: {mascota[2]} años")
+                print(f"Especie: {mascota[3]}")
+                print(f"Raza: {mascota[4]}")
+                print(f"Sexo: {'Macho' if mascota[5] == 'M' else 'Hembra'}")
+                print(f"Atención: {mascota[6]}")
+                print(f"Dueño: {mascota[7]}")
+                encontrado = True
+        if not encontrado:
+            print("Mascota no encontrada.")
+
+def filtro_gatos_cirugia():
+        print("\n--- GATOS QUE NECESITAN CIRUGÍA ---")
+        gatos_cirugia = []
+        
+        for mascota in veterinaria_alessandrello:
+            if mascota[3].lower() == 'gato' and mascota[6].lower() == 'cirugia':
+                gatos_cirugia.append(mascota)
+        
+        if gatos_cirugia:
+            print(f"{'ID':<5} {'NOMBRE':<15} {'EDAD':<5} {'RAZA':<15} {'SEXO':<10} {'DUEÑO':<15}")
+            print("-"*65)
+            for gato in gatos_cirugia:
+                sexo_str = 'Macho' if gato[5] == 'M' else 'Hembra'
+                print(f"{gato[0]:<5} {gato[1]:<15} {gato[2]:<5} {gato[4]:<15} {sexo_str:<10} {gato[7]:<15}")
+        else:
+            print("No hay gatos que necesiten cirugía en este momento.")
 
 # Programa principal
 limpiar_terminal()
@@ -83,40 +119,10 @@ while op != 5:
         agregar_mascota()
         print("Mascota(s) agregada(s) correctamente.")
     elif op == 3:
-        codigo_buscar = int(input("Ingrese el codigo ID de la mascota a buscar: "))
-        encontrado = False
-        
-        for mascota in veterinaria_alessandrello:
-            if not encontrado and mascota[0] == codigo_buscar:
-                print("\n--- HISTORIA CLÍNICA ---")
-                print(f"ID: {mascota[0]}")
-                print(f"Nombre: {mascota[1]}")
-                print(f"Edad: {mascota[2]} años")
-                print(f"Especie: {mascota[3]}")
-                print(f"Raza: {mascota[4]}")
-                print(f"Sexo: {'Macho' if mascota[5] == 'M' else 'Hembra'}")
-                print(f"Atención: {mascota[6]}")
-                print(f"Dueño: {mascota[7]}")
-                encontrado = True
-        if not encontrado:
-            print("Mascota no encontrada.")
+        buscar_mascota_id()
 
     elif op == 4:
-        print("\n--- GATOS QUE NECESITAN CIRUGÍA ---")
-        gatos_cirugia = []
-        
-        for mascota in veterinaria_alessandrello:
-            if mascota[3].lower() == 'gato' and mascota[6].lower() == 'cirugia':
-                gatos_cirugia.append(mascota)
-        
-        if gatos_cirugia:
-            print(f"{'ID':<5} {'NOMBRE':<15} {'EDAD':<5} {'RAZA':<15} {'SEXO':<10} {'DUEÑO':<15}")
-            print("-"*65)
-            for gato in gatos_cirugia:
-                sexo_str = 'Macho' if gato[5] == 'M' else 'Hembra'
-                print(f"{gato[0]:<5} {gato[1]:<15} {gato[2]:<5} {gato[4]:<15} {sexo_str:<10} {gato[7]:<15}")
-        else:
-            print("No hay gatos que necesiten cirugía en este momento.")
+        filtro_gatos_cirugia()
 
     else:
         print("Opcion no valida.")
